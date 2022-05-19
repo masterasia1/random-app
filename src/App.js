9ac7587c2afcf1a0e71c4f0d82dc815a0bd357d2
masterasia1 <a.mayfield18@gmail.com>
2022-05-19 04:18:45 -0700

    added buttons

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import Footer from './Footer';
 
 function App() {
  
- 
- 
- 
   return (
     <div className="App">
      <Header />
--- src/Content.js
+++ src/Content.js
@@ -6,12 +6,21 @@ const Content = () => {
         const int = Math.floor(Math.random() * 4);
         return names[int]
       }
+
+      const handleClick = () => {
+        console.log('click it good')
+      }
+      const handleClick2 = (name) => {
+        console.log( `${name} is great` )
+      }
       
   return (
     <main>
         <p>
         HI {handleNameChange ()}
         </p>
+        <button onClick={handleClick}>Click</button>
+        <button onClick={() => handleClick2 ('Asia')}>Click</button>
     </main>
   )
 }
